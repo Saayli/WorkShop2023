eab213b885baa9d9a7be3c5cb9b9f27c4ba0ff6a
Sayli <hessabilyas@gmail.com>
2023-10-03 10:42:31 +0200

    add hardware monitor

--- Script/main.py
+++ Script/main.py
@@ -1,8 +1,13 @@
 import wmi
+
 w = wmi.WMI(namespace="root\OpenHardwareMonitor")
 temperature_infos = w.Sensor()
 for sensor in temperature_infos:
-    if sensor.SensorType == u'Temperature' or sensor.SensorType == u'Power':
         print(sensor.Name)
         print(sensor.SensorType)
         print(sensor.Value)
+
+"""
+w_temp = wmi.WMI(namespace="root\\wmi")
+print((w_temp.MSAcpi_ThermalZoneTemperature()[0].CurrentTemperature / 10.0) - 273.15)
+"""
